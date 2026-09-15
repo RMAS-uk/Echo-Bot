d24ff808a559ded3d0a9ff33c1a59a77a27b2eee
RMAS <70452726+RMAS-uk@users.noreply.github.com>
2026-09-15 14:38:41 +0100

    bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ from discord.ext import commands
 from datetime import timedelta
 import json
 import os
+from dotenv import load_dotenv
+
+load_dotenv("/home/container/.env")
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
